20c7e1cce2dc3891efb74b1da890951b4e31950b
WojtekRok <woroztoro@gmail.com>
2019-11-12 19:41:28 +0100

    pierwsza zmiana w projekcie

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Arrays;
+import java.util.Collections;
+// sout -> system.out.println;
+// CTRL + D -> duplikowanie lini kodu
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Publikujemy pierwszy program");
 
     }
 }
